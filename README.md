db20df20316be303e758077f6ad16cc6ae8fce3c
deepakgudi-pixel <japanesen52018@gmail.com>
2021-10-12 17:48:19 +0530

    added 3D box and custom script

---
+++ README.md
@@ -0,0 +1,6 @@
+#WeBlog
+
+<li>Orbit controls</li>
+
+Orbit controls allow the camera to orbit around a target.
+To use this, as with all files in the /examples directory, you will have to include the file separately in your HTML
--- app.js
+++ app.js
@@ -1,33 +1,52 @@
-let camera, scene, renderer;
-let geometry, material, mesh;
+class Sketch{
+    constructor(options){
+        this.time = 0;
+        this.container = options.dom;
+        //create scene
+        this.scene = new THREE.Scene();
 
-init();
+       this.width = this.container.offsetWidth;
+       this.height = this.container.offsetHeight;
 
-function init() {
+        //create camera
+	    this.camera = new THREE.PerspectiveCamera( 70, this.width/this.height, 0.01, 10 );
+	    this.camera.position.z = 1;
 
-	camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
-	camera.position.z = 1;
+        this.renderer = new THREE.WebGLRenderer( { antialias: true } );
+	    this.renderer.setSize( this.width, this.height );
+	    this.container.appendChild( this.renderer.domElement );
 
-	scene = new THREE.Scene();
 
-	geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
-	material = new THREE.MeshNormalMaterial();
+        this.addObjects();
+        this.render();
+    }
 
-	mesh = new THREE.Mesh( geometry, material );
-	scene.add( mesh );
+    resize(){
 
-	renderer = new THREE.WebGLRenderer( { antialias: true } );
-	renderer.setSize( window.innerWidth, window.innerHeight );
-	renderer.setAnimationLoop( animation );
-	document.body.appendChild( renderer.domElement );
+    }
 
+    addObjects(){
+        this.geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+        this.material = new THREE.MeshNormalMaterial();
+	    this.mesh = new THREE.Mesh( this.geometry, this.material );
+	    this.scene.add( this.mesh );
+    }
+
+    render(){
+        this.time += 0.05;
+        this.mesh.rotation.x = this.time / 2000;
+        this.mesh.rotation.y = this.time / 1000;
+    
+        this.renderer.render( this.scene, this.camera );
+        this.window.requestAnimationFrame(this.render.bind(this)); 
+    } 
 }
 
-function animation( time ) {
+new Sketch({
+    dom: document.getElementById("container")
+});
 
-	mesh.rotation.x = time / 2000;
-	mesh.rotation.y = time / 1000;
 
-	renderer.render( scene, camera );
 
-}
+
+
