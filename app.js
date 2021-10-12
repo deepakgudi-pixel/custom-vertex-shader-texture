472bf4d2381d75e148f79a54eea13436446092e4
deepakgudi-pixel <japanesen52018@gmail.com>
2021-10-12 20:51:06 +0530

    added 3D box and custom script

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import * as THREE from 'https://cdn.skypack.dev/three@0.133.1';
+import { OrbitControls } from 'https://cdn.skypack.dev/three@0.133.1/examples/jsm/controls/OrbitControls.js';
+
 class Sketch{
     constructor(options){
         this.time = 0;
@@ -13,21 +16,48 @@ class Sketch{
 	    this.camera.position.z = 1;
 
         this.renderer = new THREE.WebGLRenderer( { antialias: true } );
-	    this.renderer.setSize( this.width, this.height );
+	   
 	    this.container.appendChild( this.renderer.domElement );
 
+        this.controls = new OrbitControls( this.camera, this.renderer.domElement );
 
+        this.resize();
+        this.setupResize();
         this.addObjects();
         this.render();
     }
 
-    resize(){
+    setupResize(){
+        window.addEventListener("resize", this.resize.bind(this));
+    }
 
+    resize(){
+        this.width = this.container.offsetWidth;
+        this.height = this.container.offsetHeight;
+        this.renderer.setSize( this.width, this.height ); 
+        this.camera.aspect = this.width/ this.height; 
+        //we are saying the viewport has changed and resize based on vieport
+        this.camera.updateProjectionMatrix(); 
     }
 
     addObjects(){
-        this.geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+        this.geometry = new THREE.BoxGeometry( 0.3, 0.3, 0.3 );
         this.material = new THREE.MeshNormalMaterial();
+
+        //creating shaders
+        //this shader material has couple of options 
+        this.material = new THREE.ShaderMaterial({
+            fragmentShader: `
+            void main(){
+                gl_FragColor = vec4(1, 0, 1, 1);
+            }`,
+            vertexShader: `
+                void main(){
+                    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+                }
+            `,
+        });
+
 	    this.mesh = new THREE.Mesh( this.geometry, this.material );
 	    this.scene.add( this.mesh );
     }
@@ -38,7 +68,7 @@ class Sketch{
         this.mesh.rotation.y = this.time / 1000;
     
         this.renderer.render( this.scene, this.camera );
-        this.window.requestAnimationFrame(this.render.bind(this)); 
+        window.requestAnimationFrame(this.render.bind(this)); 
     } 
 }
 
